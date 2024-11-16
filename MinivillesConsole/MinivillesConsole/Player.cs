8a974532b94cad5b18b10ac8bec441b5633f8ba9
GREBLESKEFEU <enki.devaine@gmail.com>
2024-11-16 17:25:16 +0100

    Class Player

--- MinivillesConsole/MinivillesConsole/Player.cs
+++ MinivillesConsole/MinivillesConsole/Player.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MinivillesConsole
 {
-    internal class Player
+    public class Player
     {
+        public int Coins { get; private set; }
+        public List<Card> Cards { get; }
+
+        public Player()
+        {
+            Coins = 3;
+            Cards = new List<Card>
+            {
+                new Card("Champs de blé", "Bleu", 1, "Recevez 1 pièce", 1),
+            new Card("Boulangerie", "Vert", new[] { 2, 3 }, "Recevez 2 pièces", 1)
+            };
+        }
+
+        public void AddCard(Card card)
+        {
+            Cards.Add(card);
+        }
+
+        public void AddCoins(int amount)
+        {
+            Coins += amount;
+        }
+
+        public void SubtractCoins(int amount)
+        {
+            Coins = Math.Max(0, Coins - amount);
+        }
+
+        public override string ToString()
+        {
+            string cards = string.Join(", ", Cards);
+            return $"Player 1: {Coins} pièces, Cartes: {cards}";
+        }
     }
+
 }
