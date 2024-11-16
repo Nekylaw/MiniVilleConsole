60729e670b99b4723e229055f1c94bc2bd7bd1fb
GREBLESKEFEU <enki.devaine@gmail.com>
2024-11-16 16:50:18 +0100

    Class Program

--- MinivillesConsole/MinivillesConsole/Program.cs
+++ MinivillesConsole/MinivillesConsole/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+        }
+}
