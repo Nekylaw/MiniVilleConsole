e08bca642b7e9f1e407b6282c94cdb3556d452b0
Xuatlox <asimonnet16@gmail.com>
2024-11-17 18:11:12 +0100

    Pile display

--- MinivillesConsole/MinivillesConsole/Pile.cs
+++ MinivillesConsole/MinivillesConsole/Pile.cs
@@ -20,8 +20,12 @@ namespace MinivillesConsole
 
         public void displayCards()
         {
-            
-
+            Console.WriteLine("Buy a card from the shop using one of the following commands of your choice:");
+            foreach (var cardType in decksByName.Keys)
+            {
+                Console.WriteLine($"-'{cardType}': Buy a {decksByName[cardType].name} for {decksByName[cardType].cost} coins ({decksByName[cardType].cardsLeftStore} left)");
+            }
+            Console.WriteLine("-'/': Skip the buying phase");
         }
 
         public void initializeDeck()
@@ -29,37 +33,25 @@ namespace MinivillesConsole
             List<Cards> allCards = new List<Cards>
             {
                 new Cards("WF","wheat field", 1, 1, "harvest", "blue", 1, 0, 6),
-
-                new Cards("FO","forest", 2, 1, "Naturel", "blue", 5, 0, 6),
-                new Cards("MI","mine", 6, 5, "Naturel", "blue", 9, 0, 6),
+                new Cards("FO","forest", 3, 1, "natural", "blue", 5, 0, 6),
+                new Cards("MI","mine", 6, 5, "natural", "blue", 9, 0, 6),
                 new Cards("FA","farm", 2, 1, "", "blue", 2, 0, 6),
-                new Cards("VE","verger", 3, 3, "Recolte", "blue", 10, 0, 6),
+                new Cards("VE","verger", 3, 3, "harvest", "blue", 10, 0, 6),
                 new Cards("BA","bakery", 1, 2, "shop", "green", 2, 3, 6),
                 new Cards("ST","store", 2, 3, "shop", "green", 4, 0, 6),
                 new Cards("CH","cheese factory", 5, 3, "factory", "green", 7, 0, 6),
-                new Cards("FF","furtnitures factory", 3, 3, "factory", "green", 8, 0, 6),
+                new Cards("FF","furnitures factory", 3, 3, "factory", "green", 8, 0, 6),
                 new Cards("MA"," market", 5, 2, "grocery", "green", 11, 12, 6),
-                new Cards("CO","coffee", 2, 1, "restauration", "red", 3, 0, 6),
-                new Cards("RE","restaurant", 3, 2, "restauration", "green", 9, 10, 6),
-                new Cards("BC","buisness center", 8, 0, "", "purple", 6, 0, 6), //EXCEPTION : echange d'etablissement avec un joueur
-                new Cards("TE","television", 7, 5, "etablissement", "purple", 6, 0, 6),
-                new Cards("ST","stadium", 6, 2, "etablissement", "purple", 6, 0,6),
+                new Cards("CO","coffee", 2, 1, "restaurant", "red", 3, 0, 6),
+                new Cards("RE","restaurant", 3, 2, "restaurant", "green", 9, 10, 6),
+                new Cards("BC","business center", 8, 0, "", "purple", 6, 0, 6), //EXCEPTION : echange d'etablissement avec un joueur
+                new Cards("TE","television", 7, 5, "establishment", "purple", 6, 0, 6),
+                new Cards("ST","stadium", 6, 2, "establishment", "purple", 6, 0,6),
             };
 
             // Regrouper les cartes par couleur
             foreach (Cards card in allCards)
-            {
-                if (!decksByName.ContainsKey(card.id))
-                {
-                    decksByName[card.id] = card;
-                }
-               
-            }
-
+                decksByName[card.id] = card;
         }
-
-
-
-
     }
 }
