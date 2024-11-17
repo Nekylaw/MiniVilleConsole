ffbb9bedde5af2f52bf57c293721ca80b616055d
Nekylaw <nekylaw.ao@gmail.com>
2024-11-17 16:39:50 +0100

    update pile, cards

--- MinivillesConsole/MinivillesConsole/Cards.cs
+++ MinivillesConsole/MinivillesConsole/Cards.cs
@@ -21,27 +21,28 @@ namespace MinivillesConsole
         public int anyRound;
         public int diceValue1;
         public int diceValue2;
-        public int cardsLeftStore = 6;
+        public int cardsLeftStore = 6;$
+        public string id;
 
 
 
 
         #region
-        Cards weatField = new Cards("wheat field", 1, 1, "harvest", "blue", 1, 0, 6);
-        Cards forest = new Cards("forest", 2, 1, "Naturel", "blue", 5, 0, 6);
-        Cards mine = new Cards("mine", 6, 5, "Naturel", "blue", 9, 0, 6);
-        Cards farm = new Cards("farm", 2, 1, "", "blue", 2, 0, 6);
-        Cards verger = new Cards("verger", 3, 3, "Recolte", "blue", 10, 0, 6);
-        Cards bakery = new Cards("bakery", 1, 2, "shop", "green", 2, 3, 6);
-        Cards shop = new Cards("store", 2, 3, "shop", "green", 4, 0, 6);
-        Cards cheeseFactory = new Cards("cheese factory", 5, 3, "factory", "green", 7, 0, 6);
-        Cards furnituresFactory = new Cards("Fabrique de meubles", 3, 3, "Usine", "green", 8, 0, 6);
-        Cards market = new Cards("market", 5, 2, "grocery", "green", 11, 12, 6);
-        Cards coffeeShop = new Cards("coffee", 2, 1, "restauration", "red", 3, 0, 6);
-        Cards restaurant = new Cards("restaurant", 3, 2, "restauration", "green", 9, 6, 6);
-        Cards buisnessCenter = new Cards("buisness center", 8, 0, "", "purple", 6, 0, 6); //EXCEPTION : echange d'etablissement avec un joueur
-        Cards tvChannel = new Cards("television", 7, 5, "etablissement", "purple", 6, 0, 6);
-        Cards stadium = new Cards("stadium", 6, 2, "etablissement", "purple", 6, 0, 6);
+        Cards weatField = new Cards("WF", "wheat field", 1, 1, "harvest", "blue", 1, 0, 6);
+        Cards forest = new Cards("FO", "forest", 2, 1, "Naturel", "blue", 5, 0, 6);
+        Cards mine = new Cards("MI","mine", 6, 5, "Naturel", "blue", 9, 0, 6);
+        Cards farm = new Cards("FA", "farm", 2, 1, "", "blue", 2, 0, 6);
+        Cards verger = new Cards("VE", "verger", 3, 3, "Recolte", "blue", 10, 0, 6);
+        Cards bakery = new Cards("BA", "bakery", 1, 2, "shop", "green", 2, 3, 6);
+        Cards shop = new Cards("ST","store", 2, 3, "shop", "green", 4, 0, 6);
+        Cards cheeseFactory = new Cards("CH","cheese factory", 5, 3, "factory", "green", 7, 0, 6);
+        Cards furnituresFactory = new Cards("FF","furnitures factory", 3, 3, "Usine", "green", 8, 0, 6);
+        Cards market = new Cards("MA","market", 5, 2, "grocery", "green", 11, 12, 6);
+        Cards coffeeShop = new Cards("CO","coffee", 2, 1, "restauration", "red", 3, 0, 6);
+        Cards restaurant = new Cards("RE","restaurant", 3, 2, "restauration", "green", 9, 6, 6);
+        Cards buisnessCenter = new Cards("BC","buisness center", 8, 0, "", "purple", 6, 0, 6); //EXCEPTION : echange d'etablissement avec un joueur
+        Cards tvChannel = new Cards("TE","television", 7, 5, "etablissement", "purple", 6, 0, 6);
+        Cards stadium = new Cards("ST","stadium", 6, 2, "etablissement", "purple", 6, 0, 6);
         #endregion
 
         //Cards trainStation = new Cards("train station", 4, 0, "Monument", "special", 0, 0, 0);
@@ -49,7 +50,7 @@ namespace MinivillesConsole
         //Cards mall = new Cards("mall", 10, 1, "Monument", "special", 0, 0, 0);
         //Cards radio = new Cards("radio tower", 22, 0, "Monument", "special", 0, 0, 0);
 
-        public Cards(string Name, int Cost, int GainValue, string Type, string Color, int DiceValue1, int DiceValue2, int CardsLeftStore)
+        public Cards(string Id, string Name, int Cost, int GainValue, string Type, string Color, int DiceValue1, int DiceValue2, int CardsLeftStore)
         {
             name = Name;
             cost = Cost;
@@ -59,6 +60,7 @@ namespace MinivillesConsole
             diceValue1 = DiceValue1;
             diceValue2 = DiceValue2;
             cardsLeftStore = CardsLeftStore;
+            id = Id;
 
         }
 
