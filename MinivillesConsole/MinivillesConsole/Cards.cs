4486ed6b8c63d5208d827a7ed89a2320f0544b2a
Nekylaw <nekylaw.ao@gmail.com>
2024-11-18 01:48:58 +0100

    Update Cards.cs

--- MinivillesConsole/MinivillesConsole/Cards.cs
+++ MinivillesConsole/MinivillesConsole/Cards.cs
@@ -11,45 +11,21 @@ namespace MinivillesConsole
     public class Cards
     {
 
-
+        //attributs associés à chaque carte du jeu
         public int cost;
-        public int gainValue;
+        public int gainValue; //ce que la carte permet de gagner en pièces
         public string name;
         public string type;
         public string color;
-        public int stackable;
         public int anyRound;
-        public int diceValue1;
-        public int diceValue2;
+        public int diceValue1; //première valeur possible du dé pour déclencher l'effet de la carte
+        public int diceValue2; //première valeur possible du dé pour déclencher l'effet de la carte
         public int cardsLeftStore = 6;
         public string id;
 
 
 
-
-        //#region
-        //Cards weatField = new Cards("WF", "wheat field", 1, 1, "harvest", "blue", 1, 0, 6);
-        //Cards forest = new Cards("FO", "forest", 2, 1, "Naturel", "blue", 5, 0, 6);
-        //Cards mine = new Cards("MI","mine", 6, 5, "Naturel", "blue", 9, 0, 6);
-        //Cards farm = new Cards("FA", "farm", 2, 1, "", "blue", 2, 0, 6);
-        //Cards verger = new Cards("VE", "verger", 3, 3, "Recolte", "blue", 10, 0, 6);
-        //Cards bakery = new Cards("BA", "bakery", 1, 2, "shop", "green", 2, 3, 6);
-        //Cards shop = new Cards("ST","store", 2, 3, "shop", "green", 4, 0, 6);
-        //Cards cheeseFactory = new Cards("CH","cheese factory", 5, 3, "factory", "green", 7, 0, 6);
-        //Cards furnituresFactory = new Cards("FF","furnitures factory", 3, 3, "Usine", "green", 8, 0, 6);
-        //Cards market = new Cards("MA","market", 5, 2, "grocery", "green", 11, 12, 6);
-        //Cards coffeeShop = new Cards("CO","coffee", 2, 1, "restauration", "red", 3, 0, 6);
-        //Cards restaurant = new Cards("RE","restaurant", 3, 2, "restauration", "green", 9, 6, 6);
-        //Cards buisnessCenter = new Cards("BC","buisness center", 8, 0, "", "purple", 6, 0, 6); //EXCEPTION : echange d'etablissement avec un joueur
-        //Cards tvChannel = new Cards("TE","television", 7, 5, "etablissement", "purple", 6, 0, 6);
-        //Cards stadium = new Cards("ST","stadium", 6, 2, "etablissement", "purple", 6, 0, 6);
-        //#endregion
-
-        //Cards trainStation = new Cards("train station", 4, 0, "Monument", "special", 0, 0, 0);
-        //Cards parc = new Cards("park", 16, 0, "Monument", "special", 0, 0, 0);
-        //Cards mall = new Cards("mall", 10, 1, "Monument", "special", 0, 0, 0);
-        //Cards radio = new Cards("radio tower", 22, 0, "Monument", "special", 0, 0, 0);
-
+        
         public Cards(string Id, string Name, int Cost, int GainValue, string Type, string Color, int DiceValue1, int DiceValue2, int CardsLeftStore)
         {
             name = Name;
@@ -64,22 +40,10 @@ namespace MinivillesConsole
 
         }
 
-
-
-      
-        List<Cards> allBuildings = new List<Cards>();
-
-
-        public List<Cards> listBuildings = new List<Cards>();
-
-
-
-        //La fonction qui g�re les effets des cartes hors monuments
-
-        //CardPlayed correspond � la carte jou� pendant le tour actuel (est d�finit dans la classe player)
-        public void Effect(Player playerSendingEffect, Player playerReceivingEffect)
+        //La fonction qui gère les effets des cartes hors monuments
+        public void Effect(Player playerSendingEffect, Player playerReceivingEffect) //playerSendingEffect correspond au joueur qui lance les dés et playerReceivingEffect tous les autres joueurs 
         {
-
+            //initalisation de l'effet des cartes en fonction de leur couleur : on ajoute des pièces au joueur actuel parfois au détriment d'autres joueurs
             if (this.color == "blue")
             {
 
@@ -107,11 +71,11 @@ namespace MinivillesConsole
                     Console.WriteLine($"\n {playerSendingEffect.name} reçoit {this.gainValue} pièces de {this.name}");
                     Console.ForegroundColor = ConsoleColor.White;
                 }
-                else
+                else //cas spécial des cartes vertes qui ajoutent un nombre de pièces spécifiques pour certains types de cartes
                 {
                     int gain = 0;
                     string researched = "";
-                    switch (this.name)
+                    switch (this.name) //condition sur le nom des cartes vertes spéciales
                     {
                         case "usine a fromage":
                             researched = "breeding";
@@ -123,11 +87,11 @@ namespace MinivillesConsole
                             researched = "harvest";
                             break;
                     }
-                    foreach (var card in playerSendingEffect.cardsOwned)
+                    foreach (var card in playerSendingEffect.cardsOwned) //on parcours la main du joueur qui vient de lancer les dés
                     {
-                        if (card.type == researched)
+                        if (card.type == researched) //si le joueur a des cartes au type affecté, on ajoute pour chacune d'entre elle le montant du gain de la carte verte spécial
                         {
-                            gain += this.gainValue;
+                            gain += this.gainValue; 
                         }
                     }
                     playerSendingEffect.coins += gain;
@@ -137,51 +101,10 @@ namespace MinivillesConsole
                 }
 
             }
-            /*if (this.color == "Purple")
-            {
-                 
-                playerSendingEffect.coins += this.gainValue;
-                playerReceivingEffect.coins -= this.gainValue;
-                
-            }*/
+            
 
         }
-        //G�re les effets des monuments
-        //public void monumentEffect(Player player)
-        //{
 
-        //    foreach (var monument in player.unlockedMonuments)
-        //    {
-        //        if (monument.name == "train station")
-        //        {
-        //            Console.WriteLine($"{player} peut lancer 2 dés.");
-        //        }
-        //        if (monument.name == "radio tower")
-        //        {
-        //            player.canReroll = true;
-        //        }
-        //        if (monument.name == "mall")
-        //        {
-        //            foreach (var card in player.cardsOwned)
-        //            {
-        //                if (card.type == "restauration" || card.type == "shop")
-        //                {
-        //                    card.gainValue += 1;
-        //                }
-        //            }
-
-        //        }
-        //        if (monument.name == "park")
-        //        {
-        //            /*if (player.dices[0].face == player.dices[1].face) //faire le truc pour le doublon de dés 
-        //            {
-        //                player.canReroll = true;
-        //            }*/
-        //        }
-
-        //    }
-
-        //}
 
 
 
